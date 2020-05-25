efa883a38c2c7b6b66497eb0957aa74e8abfda47
Gabor Cserto <csertog@gmail.com>
2020-05-25 12:04:08 +0200

    Section 9 es6 modules

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,6 @@
 // Global app controller
+import str from './models/Search';
+//import { add as a, multiply as m, ID } from "./views/searchView";
+import * as searcView from "./views/searchView";
+
+console.log(`Using imported functions! ${searcView.add( searcView.ID, 2)} and ${searcView.multiply(3,5)}. ${str}`);
